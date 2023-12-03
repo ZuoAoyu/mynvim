2410f4d1c0046ab28dd4598ef2dc466ca2a262ab
aoyu <aoyu@xiake.me>
2023-12-03 14:39:19 +0800

    使用mason-lspconfig的automatic server setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,17 +19,8 @@ require("mason-lspconfig").setup({
   },
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-require("lspconfig").lua_ls.setup {
-  capabilities = capabilities,
-}
-
 require("mason-lspconfig").setup_handlers({
   function (server_name)
-    require("lspconfig")[server_name].setup{}
+    require("lspconfig")[server_name].setup {}
   end,
-  ["clangd"] = function ()
-    require("lspconfig").clangd.setup {}
-  end
 })
